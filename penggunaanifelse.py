ec522e53c8e187c12a15981041aab678dee55910
Hilmi Khaidar N <101361677+HilmiKhaidar@users.noreply.github.com>
2024-11-08 03:57:40 +0700

    penggunaan if else

---
+++ penggunaanifelse.py
@@ -0,0 +1,16 @@
+# Contoh penggunaan if else
+nilai = int(input("Masukkan nilai ujian: "))
+
+if nilai >= 75:
+    print("Selamat, Anda lulus!")
+else:
+    print("Maaf, Anda tidak lulus.")
+
+
+
+nilai = int(input("Masukan nilai ujian"))
+
+if nilai <= 75:
+    print("Selamat, Anda lulus!!")
+else:
+    print("Maaf, Anda tidak lulus!")
